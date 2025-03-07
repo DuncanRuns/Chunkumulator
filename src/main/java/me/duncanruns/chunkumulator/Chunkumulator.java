7a7515bf14bdb154a3983010c6440e6930709011
Duncan <59705125+DuncanRuns@users.noreply.github.com>
2025-03-07 15:56:00 -0400

    v1.1.0

--- src/main/java/me/duncanruns/chunkumulator/Chunkumulator.java
+++ src/main/java/me/duncanruns/chunkumulator/Chunkumulator.java
@@ -7,6 +7,7 @@ import org.apache.logging.log4j.Logger;
 
 public class Chunkumulator {
     public static final Logger LOGGER = LogManager.getLogger("chunkumulator");
+    public static final long CHUNKUMULATOR_KEEPALIVE_ID = -1457264210 /*-Math.abs("Chunkumulator".hashCode())*/;
 
     public static PlayerChunkAccumulator getChunkQueueFromPlayer(ServerPlayerEntity player) {
         return ((ServerPlayerEntityInt) player).chunkumulator$getChunkQueue();
